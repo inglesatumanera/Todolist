38303ab98adc7c218aa4b1a146cff08d4085b451
inglesatumanera <spanishenglishlearningai@gmail.com>
2025-08-14 02:15:58 -0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by ralphhhh on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
